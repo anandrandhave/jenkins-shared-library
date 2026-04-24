de6527f3da6ec4cf4364ad8e3bda2e319dd2cc26
Anand Randhave <anand.randhave@gmail.com>
2026-04-24 14:36:47 +0530

    Assignment 3: Full pipeline update with test and report fixes

---
+++ src/test/java/com/devops/assignment/UnitTest.java
@@ -0,0 +1,10 @@
+package com.devops.assignment;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UnitTest {
+    @Test
+    public void demoTest() {
+        assertTrue(true);
+    }
+}
